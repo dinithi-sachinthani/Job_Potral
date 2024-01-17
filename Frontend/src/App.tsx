be55ed33996704ec5ebc8492e5b1be3283e215c4
e18264 <e18264@eng.pdn.ac.lk>
2024-01-17 11:32:08 +0530

    Add routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import JobSeekerProfile from "./pages/JobSeekerProfile";
+import CompanyProfile from "./pages/CompanyProfile";
+import Admin from "./pages/Admin";
+import NoPage from "./pages/NoPage";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/jobseeker" element={<JobSeekerProfile />} />
+          <Route path="/company" element={<CompanyProfile />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ Frontend/src/pages/CompanyProfile.tsx
@@ -0,0 +1,7 @@
+export default function CompanyProfile() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">CompanyProfile!!!!!</h1>
+    </>
+  );
+}
