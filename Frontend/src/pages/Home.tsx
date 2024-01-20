e6dc3b193c107ecdab82001accf57ecbbf6d2e62
e18264 <e18264@eng.pdn.ac.lk>
2024-01-21 00:28:57 +0530

    complete login page

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Logo from "../assets/dummy-logo.png";
 import ProfAvatar from "../assets/profile-avatar.jpg";
 
@@ -32,7 +33,9 @@ export default function Home() {
   return (
     <div className="relative bg-neutral-200 h-screen pt-14 flex flex-col justify-start">
       <div className="menu-bar w-full h-14 bg-slate-50 flex flex-row fixed top-0 left-0 justify-between px-20">
-        <img src={Logo} alt="dummy-logo" style={{ height: 50 }} />
+        <Link to="/">
+          <img src={Logo} alt="dummy-logo" style={{ height: 50 }} />
+        </Link>
         <div className="avatar my-auto flex flex-row">
           <Avatar alt="Remy Sharp" src={ProfAvatar} />
           <MenuComponent />
